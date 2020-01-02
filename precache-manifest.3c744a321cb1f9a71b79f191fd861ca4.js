fdddab6c4912702792d8a85586be79edfba52652
smjcrane <smjcrane@gmail.com>
2020-01-02 15:43:18 +0000

    Updates

---
+++ precache-manifest.3c744a321cb1f9a71b79f191fd861ca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a16e159ef89f70606d4bd54c5a1be7",
+    "url": "/mazegame/index.html"
+  },
+  {
+    "revision": "0a72058473e80d748874",
+    "url": "/mazegame/static/js/2.8b89f785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mazegame/static/js/2.8b89f785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0691c7a654c444b88fa6",
+    "url": "/mazegame/static/js/main.b22b122a.chunk.js"
+  },
+  {
+    "revision": "dea3b6cafb2f9717ee34",
+    "url": "/mazegame/static/js/runtime-main.3ba3e282.js"
+  }
+]);
